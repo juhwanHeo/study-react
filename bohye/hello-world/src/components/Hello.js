b0067e89eebdca19df741ad1463f526e6b91779c
bobo-h <bo_asapap@kakao.com>
2024-07-14 17:56:41 +0900

    feat: 메뉴 컴포넌트 생성 및 링크 형성

--- bohye/hello-world/src/components/Hello.js
+++ bohye/hello-world/src/components/Hello.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Menu from "./Menu";
 
 const Hello = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <div>Hello World</div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Hello;
--- bohye/hello-world/src/components/Menu.js
+++ bohye/hello-world/src/components/Menu.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Menu = () => {
-  return <div></div>;
+  return (
+    <div style={{ borderTop: "1px solid grey" }}>
+      <a
+        href="https://www.google.co.kr/?hl=ko"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Google
+      </a>
+      <br />
+      <a href="https://www.naver.com/" target="_blank" rel="noreferrer">
+        Naver
+      </a>
+    </div>
+  );
 };
 
 export default Menu;
