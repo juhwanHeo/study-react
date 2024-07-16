e12f660eb15733ca1e64ebe17257f4b3192ecfcd
juwonlee-dev <juwonlee.dev@gmail.com>
2024-07-17 02:18:53 +0900

    fix: fix warning in Menu component

--- juwonlee/hello-world/src/components/Menu/index.js
+++ juwonlee/hello-world/src/components/Menu/index.js
@@ -2,7 +2,7 @@ import  './Menu.css'
 
 const Menu = ({ title, url }) => {
     return (
-        <a class="menu-item" href={ url } target="_blank">{ title }</a>
+        <a className={ "menu-item" } href={ url } target="_blank">{ title }</a>
     );
 }
 
