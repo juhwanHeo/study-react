4e5a61ab2509eaea967c52a681f2f119c970bdd2
MIK_WJ <congbyrr@gmail.com>
2024-07-17 00:24:03 +0900

    feat: add Menu Component (nav)

--- ujin/hello-world/src/index.js
+++ ujin/hello-world/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Hello from './components/Hello'
+import Menu from './components/Menu'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Hello/>
+    <Menu/>
   </React.StrictMode>
 );
 
