fffa41f82b7e721917cb033257431a5f1ff8ee86
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-09-26 22:46:31 +0900

    fix

--- bohye/board-project/src/App.js
+++ bohye/board-project/src/App.js
@@ -5,8 +5,7 @@ import "./App.css";
 const fetchData = async () => {
   try {
     const response = await fetch("http://heojh.iptime.org:8003/board");
-    const data = await response.json();
-    return data;
+    return await response.json();
   } catch (error) {
     console.error("Error fetching data:", error);
     return [];
