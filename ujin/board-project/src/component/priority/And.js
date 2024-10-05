bda49f6da9c8d14eb1307456648df235ddecac68
MIK_WJ <congbyrr@gmail.com>
2024-10-05 19:25:25 +0900

    Add crypto at Board.js and priority dir and files

---
+++ ujin/board-project/src/component/priority/And.js
@@ -0,0 +1,3 @@
+console.log(true || false && false)
+console.log(true && (false || false))
+console.log(2 === 3 || (4 < 0 && 1 === 1))
---
+++ ujin/board-project/src/component/priority/OR.js
@@ -0,0 +1,2 @@
+console.log(true || false && false)
+console.log((true || false) && false)
