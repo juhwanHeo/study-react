90bd9df98a6a0d03cf85ec881c49beb86a95c709
MIK_WJ <congbyrr@gmail.com>
2024-10-18 01:06:46 +0900

    feat: add priority file and add index.js

--- ujin/board-project/src/component/priority/And.js
+++ ujin/board-project/src/component/priority/And.js
@@ -1,3 +1,24 @@
-console.log(true || false && false)
-console.log(true && (false || false))
-console.log(2 === 3 || (4 < 0 && 1 === 1))
+function And() {
+    const array = [
+        "true || false && false",
+        "true && (false || false)",
+        "2 === 3 || (4 < 0 && 1 === 1)"
+    ]
+    const list = []
+
+    array.forEach((item) => {
+        const result = eval(item)
+        list.push(
+            <div>
+                <span>{item} = {`${result}`}</span>
+            </div>
+        )
+    })
+    return (
+        <div>
+            { list }
+        </div>
+    );
+}
+
+export default And;
