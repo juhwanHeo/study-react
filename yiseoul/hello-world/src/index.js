fda5d83ea73257ea22265e56f2cc3ddf60fed78e
LSHgy <his5423@gmail.com>
2024-10-23 01:05:52 +0900

    feat: Add hello, menu components

--- yiseoul/hello-world/src/index.js
+++ yiseoul/hello-world/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Hello} from "./components/Hello";
+import {Menu} from "./components/Menu";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Hello/>
+        <hr/>
+
+        <Menu
+            link={"https://www.naver.com"}
+            name={"네이버"}
+        />
+
+        <Menu
+            link={"https://www.google.com"}
+            name={"구글"}
+        />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
