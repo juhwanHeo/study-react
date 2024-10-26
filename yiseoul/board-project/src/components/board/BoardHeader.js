8f94bd0133df37445a40e51a89618f7a6634ecf9
LSHgy <his5423@gmail.com>
2024-10-27 04:46:45 +0900

    feat: Add BoardHeader componenets
    - Board 컴포넌트의 헤더 파일 추가
    
    Related to #8

---
+++ yiseoul/board-project/src/components/board/BoardHeader.js
@@ -0,0 +1,11 @@
+function BoardHeader() {
+	return (
+		<div className="board-header">
+			<div className="board-header-item">Seq</div>
+			<div className="board-header-item">Title</div>
+			<div className="board-header-item">Creator</div>
+		</div>
+	);
+}
+
+export default BoardHeader;
