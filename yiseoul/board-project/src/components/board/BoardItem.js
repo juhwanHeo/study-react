09b2e2b96e139fd8c01c3e57f87e306f7d98b448
LSHgy <his5423@gmail.com>
2024-10-27 04:47:45 +0900

    feat: Add BoardItem componenets
    - Board 컴포넌트의 테이블 body 영역 추가
    - 컨텐츠가 없을 때의 function 추가
    
    Related to #8

---
+++ yiseoul/board-project/src/components/board/BoardItem.js
@@ -0,0 +1,29 @@
+import './board.css';
+
+function NoItem() {
+	return (
+		<div className="board-row ful">
+			<span>No Contents</span>
+		</div>
+	)
+}
+
+function BoardItem({ items }) {
+	return (
+		<>
+			{items.length === 0 ? (
+				<NoItem />
+			) : (
+				items.map(({ seq, title, creator }, index) => (
+					<div className="board-row" key={index}>
+						<div className="board-item">{seq}</div>
+						<div className="board-item">{title}</div>
+						<div className="board-item">{creator}</div>
+					</div>
+				))
+			)}
+		</>
+	);
+}
+
+export default BoardItem;
