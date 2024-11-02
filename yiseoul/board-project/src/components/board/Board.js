2cee4e9337861d1835a9b301c86512636940ab8a
LSHgy <his5423@gmail.com>
2024-11-03 07:00:43 +0900

    feat: create, update, delete 컴포넌트 사용 및 관련 이벤트 추가

--- yiseoul/board-project/src/components/board/Board.js
+++ yiseoul/board-project/src/components/board/Board.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import BoardHeader from './BoardHeader';
 import BoardItem from './BoardItem';
+import Create from "./cud/Create";
 
-function Board({boardItems}) {
+function Board({boardItems, useCreate, useDelete, useEdit}) {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -10,12 +11,36 @@ function Board({boardItems}) {
     setItems(validItems.map((item) => ({key: crypto.randomUUID(), ...item})));
   }, [boardItems]);
 
+  const handleEdit = (key, { title }) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.key === key ? { ...item, title: title } : item
+      )
+    );
+  };
+
+  const handleDelete = (key) => {
+    setItems(items.filter(item => item.key !== key));
+  };
+
+  const handleCreate = (newItem) => {
+    const newSeq = items.length ? items[items.length - 1].seq + 1 : 1;
+    newItem = {
+      key: crypto.randomUUID(),
+      seq: newSeq,
+      title: newItem.title || '',
+      creator: newItem.creator || '',
+    };
+    setItems([...items, newItem]);
+  };
+
 
   return (
-    <div className="board">
-      <BoardHeader />
-      <BoardItem items={items} />
-    </div>
+      <div className="board">
+        <BoardHeader/>
+        <BoardItem items={items} onDelete={handleDelete} onUpdate={handleEdit} />
+        <Create event={handleCreate}></Create>
+      </div>
   );
 }
 
