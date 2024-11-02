901d714734e83f154e0531196ddb4024003fe31c
LSHgy <his5423@gmail.com>
2024-11-03 04:48:03 +0900

    feat: Add useRemoteItems function
    - 데이터를 가지고 오는 함수 추가

--- yiseoul/board-project/src/components/board/index.js
+++ yiseoul/board-project/src/components/board/index.js
@@ -1,4 +1,26 @@
-import { RemoteBoard, Board } from './Board';
+import Board from './Board';
+import {useEffect, useState} from "react";
+
+const useRemoteItems  = () => {
+  const [remoteItems, setRemoteItems] = useState([]);
+
+  useEffect(() => {
+    const fetchRemoteItems = async () => {
+      try {
+        const response = await fetch("http://heojh.iptime.org:8003/board");
+        const data = await response.json();
+        setRemoteItems(data);
+      } catch (e) {
+        console.error('fetch error:', e);
+        setRemoteItems([]);
+      }
+    };
+
+    fetchRemoteItems();
+  }, []);
+
+  return remoteItems;
+}
 
 export default Board;
-export { Board, RemoteBoard };
+export {useRemoteItems};
