46972ca035cb0650681bf2f954dfee144b932e5f
LSHgy <his5423@gmail.com>
2024-11-03 06:58:33 +0900

    chore: App.js에서 필요없는 board 컴포넌트 제거

--- yiseoul/board-project/src/App.js
+++ yiseoul/board-project/src/App.js
@@ -3,17 +3,10 @@ import React, {useEffect, useState} from "react";
 import Board, {useRemoteItems } from "./components/board";
 
 function App() {
-  const items = [
-    {seq: 1, title: 'title 1', content: 'content 1', creator: 'creator 1'},
-    {seq: 2, title: 'title 2', content: 'content 2', creator: 'creator 2'},
-    {seq: 3, title: 'title 3', content: 'content 3', creator: 'creator 3'},
-  ]
   const remoteItems = useRemoteItems();
 
   return (
     <div className="App">
-      <Board boardItems={items} />
-      <br/>
       <Board boardItems={remoteItems} />
     </div>
   );
