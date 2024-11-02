b6161599a3d9657f3056ae50a6c1230fb9aceceb
LSHgy <his5423@gmail.com>
2024-11-03 03:18:20 +0900

    style: Edit indent

--- yiseoul/board-project/src/components/board/BoardItem.js
+++ yiseoul/board-project/src/components/board/BoardItem.js
@@ -1,29 +1,29 @@
 import './board.css';
 
 function NoItem() {
-    return (
-        <div className="board-row ful">
-            <span>No Contents</span>
-        </div>
-    );
+  return (
+    <div className="board-row ful">
+      <span>No Contents</span>
+    </div>
+  );
 }
 
 function BoardItem({items}) {
-    return (
-        <>
-            {items.length === 0 ? (
-                <NoItem />
-            ) : (
-                items.map(({seq, title, creator}, index) => (
-                    <div className="board-row" key={seq}>
-                        <div className="board-item">{seq || index + 1}</div>
-                        <div className="board-item">{title}</div>
-                        <div className="board-item">{creator}</div>
-                    </div>
-                ))
-            )}
-        </>
-    );
+  return (
+    <>
+      {items.length === 0 ? (
+        <NoItem />
+      ) : (
+        items.map(({seq, title, creator}, index) => (
+          <div className="board-row" key={seq}>
+            <div className="board-item">{seq || index + 1}</div>
+            <div className="board-item">{title}</div>
+            <div className="board-item">{creator}</div>
+          </div>
+        ))
+      )}
+    </>
+  );
 }
 
 export default BoardItem;
