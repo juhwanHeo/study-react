565e58befea74db9d0b7e62baf920d68412bdd9c
LSHgy <his5423@gmail.com>
2024-11-17 01:39:31 +0900

    feat: Edit Update function
    - Update의 데이터 처리 방식 비제어 컴포넌트 형식으로 변경 (useRef 사용)

--- yiseoul/board-project/src/components/board/cud/Update.js
+++ yiseoul/board-project/src/components/board/cud/Update.js
@@ -1,21 +1,24 @@
-import {useEffect, useState} from "react";
+import {useRef, useState} from "react";
 
 const Update = ({event, row}) => {
   const [toggle, setToggle] = useState(false);
-  const [item, setItem] = useState({title: row.title || ''});
+  // const [item, setItem] = useState({title: row.title || ''});
+  const inputRef = useRef(null);
 
-  useEffect(() => {
-    if (!toggle) {
-      setItem({title: row.title || ''});
-    }
-  }, [toggle]);
+  // useEffect(() => {
+  //   if (!toggle) {
+  //     setItem({title: row.title || ''});
+  //   }
+  // }, [toggle]);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setItem((prevItem) => ({
-      ...prevItem,
-      [name]: value,
-    }));
+    setToggle((prev) => !prev);
+    event(row.key, { title: inputRef.current.value });
+    // const { name, value } = e.target;
+    // setItem((prevItem) => ({
+    //   ...prevItem,
+    //   [name]: value,
+    // }));
   };
 
   return (
@@ -30,15 +33,12 @@ const Update = ({event, row}) => {
                     type="text"
                     name="title"
                     placeholder="제목"
-                    value={item.title}
-                    onChange={handleChange}
+                    defaultValue={row.title}
+                    ref={inputRef}
                 />
               </div>
               <div className="board-item row">
-                <button onClick={() => {
-                  event(row.key, item);  // 이벤트 함수 실행 (전달된 데이터를 처리)
-                  setToggle((prev) => !prev); // toggle 상태 전환
-                }}>확인</button>
+                <button onClick={handleChange}>확인</button>
               </div>
             </div>
         )}
