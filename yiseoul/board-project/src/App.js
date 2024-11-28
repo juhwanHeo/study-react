0849c8d2699ad8c4ad5a5d58bd427cd551b1b9fc
LSHgy <his5423@gmail.com>
2024-11-29 00:54:55 +0900

    chore: Edit App.js to remove unnecessary Accordion components
    - Removed unused Accordion components to clean up App.js

--- yiseoul/board-project/src/App.js
+++ yiseoul/board-project/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
 import Board, {useRemoteItems } from "./components/board";
-import Accordion from "./components/up-state/Accodion";
 
 function App() {
   const remoteItems = useRemoteItems();
 
   return (
     <div className="App">
-      <Accordion />
-      <hr/>
       <Board boardItems={remoteItems} />
     </div>
   );
