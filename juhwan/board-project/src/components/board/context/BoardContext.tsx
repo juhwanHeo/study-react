4b5cbb5ea158dadde6d0dac05ffd412b1e05dcb8
juhwanHeo <juhwanheo.dev@gmail.com>
2024-12-08 18:49:53 +0900

    feat: Add board dispatch

--- juhwan/board-project/src/components/board/context/BoardContext.tsx
+++ juhwan/board-project/src/components/board/context/BoardContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, Dispatch } from 'react';
 
 export interface BoardItem {
     id: number;
@@ -8,4 +8,21 @@ export interface BoardItem {
     creator: string;
 }
 
+enum BoardActionType {
+    SEARCH_BOARD_ITEM = "SEARCH_BOARD_ITEM",
+    CREATE_BOARD_ITEM = "CREATE_BOARD_ITEM",
+    UPDATE_BOARD_ITEM = "UPDATE_BOARD_ITEM",
+    DELETE_BOARD_ITEM = "DELETE_BOARD_ITEM",
+}
+
+type BoardAction =
+    | { type : BoardActionType.SEARCH_BOARD_ITEM, boardItems: Array<BoardItem> }
+    | { type : BoardActionType.CREATE_BOARD_ITEM, boardItem: BoardItem }
+    | { type : BoardActionType.UPDATE_BOARD_ITEM, id: number, boardItem: BoardItem }
+    | { type : BoardActionType.DELETE_BOARD_ITEM, id: number }
+    ;
+
+type BoardDispatch = Dispatch<BoardAction>;
+
 export const BoardContext = createContext<Array<BoardItem> | null>(null);
+export const BoardDispatchContext = createContext<BoardDispatch | null>(null);
