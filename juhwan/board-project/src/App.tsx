a8c0cf176009223dd57eb9e8aa43cf1992e09929
juhwanHeo <juhwanheo.dev@gmail.com>
2024-12-16 16:52:22 +0900

    feat: Update how to open to the modal
    - Update props propagation to context

--- juhwan/board-project/src/App.tsx
+++ juhwan/board-project/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Board from "./components/board/Board";
 import { BoardProvider } from './context/BoardContext';
+import { ModalProvider } from './context/ModalContext';
 
 function App() {
   return (
     <div className="App">
       <BoardProvider>
-        <Board />
+        <ModalProvider>
+          <Board />
+        </ModalProvider>
       </BoardProvider>
     </div>
   );
--- juhwan/board-project/src/components/board/Board.tsx
+++ juhwan/board-project/src/components/board/Board.tsx
@@ -1,16 +1,14 @@
 import BoardItem from "./BoardItem";
-import './css/board-style.css';
-import {useContext, useState} from "react";
-import Modal, {ModalProps} from "./cud/Modal";
-import { BoardContext } from "./context/BoardContext";
+import "./css/board-style.css";
+import { useContext } from "react";
+import Modal from "./cud/Modal";
+import { BoardContext } from "../../context/BoardContext";
 import Search from "./Search";
-
-export enum Mode {
-  NONE,
-  CREATE,
-  UPDATE,
-  DELETE,
-}
+import {
+  ModalActionType,
+  ModalDispatchContext,
+  Mode,
+} from "../../context/ModalContext";
 
 const BoardHeader = () => {
   return (
@@ -21,81 +19,52 @@ const BoardHeader = () => {
       <div>etc</div>
     </div>
   );
-}
+};
 
 const NoContent = () => {
-  return (
-    <div>No contents...</div>
-  );
-}
+  return <div>No contents...</div>;
+};
 
 const Board = () => {
-  const [modal, setModal] = useState<ModalProps>({mode: Mode.NONE});
   const boardItems = useContext(BoardContext) || [];
-
-  const createItem = (title: string, creator: string) => {
-    let index = boardItems.length + 1;
-    const newItem = {
-      id: index,
-      title: title,
-      creator: creator,
-    }
-  }
-
-  const updateItem = (id: number, title: string) => {
-    const newBoardItems = [...boardItems];
-
-    for (let i = 0; i < newBoardItems.length; i++) {
-      if (newBoardItems[i].id === id) {
-        newBoardItems[i].title = title;
-        break;
-      }
-    }
-  }
-
-  const removeItem = (id: number) => {
-  }
-
-  const onClose = () => {
-    setModal({mode: Mode.NONE});
-  }
+  const modalDispatch = useContext(ModalDispatchContext);
 
   return (
     <>
-      <Search/>
+      <Search />
       <div className={"board-wrapper"}>
-        <BoardHeader/>
-        {
-          boardItems?.length === 0
-            ? <NoContent/>
-            : boardItems.map((item, idx) => (
-                <BoardItem
-                    key={crypto.randomUUID()}
-                    id={item.id}
-                    seq={idx}
-                    title={item.title}
-                    creator={item.creator}
-                    setModal={setModal}
-                />
-              )
-            )
-        }
-        <div onClick={() => setModal({mode: Mode.CREATE})}>Create</div>
+        <BoardHeader />
+        {boardItems?.length === 0 ? (
+          <NoContent />
+        ) : (
+          boardItems.map((item, idx) => (
+            <BoardItem
+              key={crypto.randomUUID()}
+              id={item.id || 0}
+              seq={idx}
+              title={item.title || ''}
+              creator={item.creator || ''}
+            />
+          ))
+        )}
+        <div
+          onClick={() => {
+            // create modal
+            modalDispatch?.({
+              type: ModalActionType.OPEN_MODAL,
+              modalItem: {
+                open: true,
+                mode: Mode.CREATE,
+              },
+            });
+          }}
+        >
+          Create
+        </div>
       </div>
-
-      { modal?.mode !==  Mode.NONE
-          ? <Modal
-              mode={modal.mode}
-              boardItem={modal.boardItem}
-              create={createItem}
-              update={updateItem}
-              remove={removeItem}
-              onClose={onClose}
-          />
-          : null
-      }
+      <Modal />
     </>
   );
-}
+};
 
 export default Board;
