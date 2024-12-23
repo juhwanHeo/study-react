2fcf9fdbbdc46cfacbf22fea61b03b4812e12b03
LSHgy <his5423@gmail.com>
2024-12-24 01:12:52 +0900

    feat: reducer와 context로 대체되는 handler 제거 작업

--- yiseoul/board-project/src/components/board/Board.js
+++ yiseoul/board-project/src/components/board/Board.js
@@ -4,7 +4,7 @@ import BoardItem from './BoardItem';
 import Create from "./cud/Create";
 import Search from "./search";
 
-function Board({boardItems, useCreate, useDelete, useEdit}) {
+function Board({boardItems}) {
   const [items, setItems] = useState([]);
   const [filterItems, setFilterItems] = useState([]);
   const [inqValue, setInqValue] = useState('');
@@ -21,40 +21,15 @@ function Board({boardItems, useCreate, useDelete, useEdit}) {
     setFilterItems(() => { return items.filter(({title}) => !inqValue || title.includes(inqValue)); });
   }, [items, inqValue]);
 
-  const handleEdit = (key, { title }) => {
-    setItems((prevItems) =>
-      prevItems.map((item) =>
-        item.key === key ? { ...item, title: title } : item
-      )
-    );
-  };
-
-  const handleDelete = (key) => {
-    setItems(items.filter(item => item.key !== key));
-  };
-
-  const handleCreate = (newItem) => {
-    const newSeq = items.length ? items[items.length - 1].seq + 1 : 1;
-    newItem = {
-      key: crypto.randomUUID(),
-      seq: newSeq,
-      title: newItem.title || '',
-      creator: newItem.creator || '',
-    };
-    setItems([...items, newItem]);
-  };
-
-  const handleSearch = (value) => {
-    setInqValue(value);
-  };
+  const handleSearch = (value) => setInqValue(value);
 
   return (
     <div className="board-container">
       <Search event={handleSearch} value={inqValue} />
       <div className="board">
         <BoardHeader />
-        <BoardItem items={filterItems} onDelete={handleDelete} onUpdate={handleEdit} />
-        <Create event={handleCreate} />
+        <BoardItem items={filterItems} />
+        <Create />
       </div>
     </div>
   );
