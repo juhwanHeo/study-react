867305acce085b863a8765ef320fffbfe1415910
MIK_WJ <congbyrr@gmail.com>
2024-12-29 02:45:59 +0900

    Feat: Convert Board using Reducer and Context, Remove unuse things

---
+++ ujin/board-project/src/components/Board/Board.js
@@ -0,0 +1,56 @@
+import { useEffect } from 'react';
+import "./Board.css";
+import Search from './Search';
+import Create from '../cud/Create';
+import BoardItems from './BoardItems';
+import { useBoardDispatch, useBoardItems } from './Context/BoardContext';
+
+
+
+function Board() {
+    const boardList = useBoardItems();
+    const dispatch = useBoardDispatch();
+
+    useEffect(() => {
+        getBoardItems()
+        .then(res => {
+            res.map((item) => {
+                item.id = window.crypto.randomUUID()
+            })
+            dispatch({
+                    type: 'INIT',
+                    initData: res
+            });
+        })
+    }, [])
+
+    return(
+        <div>
+            <section className="searchBar">
+                <Search setItems={ boardList } />
+            </section>
+            <section className="board">
+                <div className="board-header">
+                    <h1 onClick={()=> {
+                    }}> 
+                        ID | Title | Creator | etc
+                    </h1>
+                    <div>
+                    </div>
+                </div>
+                <div>
+                    <BoardItems items={boardList}></BoardItems>
+                </div>
+            </section>
+            <Create/>
+        </div>
+        
+    );
+}
+
+async function getBoardItems() {
+    const response = await fetch('http://heojh.iptime.org:8003/board');
+    return response.json();
+}  
+
+export default Board;
--- ujin/board-project/src/components/cud/Create.js
+++ ujin/board-project/src/components/cud/Create.js
@@ -1,10 +1,16 @@
-import "../Board.css";
+import "../Board/Board.css";
+import { useBoardDispatch } from "../Board/Context/BoardContext";
 
 function Create({ onClick }) {
+    const dispatch = useBoardDispatch();
 
     return (
         <div>
-            <button className="post-button" onClick={ onClick }>
+            <button className="post-button" onClick={ () => {
+                dispatch({
+                    type: 'Create'
+                });
+            }}>
                 생성
             </button>
         </div>
