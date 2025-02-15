b11b9d040de03117ac400dbbbce6e4fb64b47456
juhwanHeo <juhwanheo.dev@gmail.com>
2025-02-16 01:34:07 +0900

    feat: Resolve Task2

--- juhwan/board-project/src/App.tsx
+++ juhwan/board-project/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Board from "./components/board/Board";
 import Task1 from './components/week8/task1/Task1';
+import Task2 from './components/week8/task2/Task2';
 import { BoardProvider } from './context/BoardContext';
 import { ModalProvider } from './context/ModalContext';
 
@@ -12,7 +13,12 @@ function App() {
           <Board />
         </ModalProvider>
       </BoardProvider> */}
-      <Task1 />
+      <div>
+        <Task1 />
+      </div>
+      <div>
+        <Task2 />
+      </div>
     </div>
   );
 }
