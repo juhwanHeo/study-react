481895df9bae82f3190b96a92d3df51c74eeb4f5
juhwanHeo <juhwanheo.dev@gmail.com>
2025-02-16 01:32:23 +0900

    feat: clone task2

---
+++ juhwan/board-project/src/components/week8/task2/Task2.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+export default function Task2() {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(c => c + increment);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+
+  return (
+    <>
+      <h1>
+        카운터: {count}
+        <button onClick={() => setCount(0)}>재설정</button>
+      </h1>
+      <hr />
+      <p>
+        초당 증가량:
+        <button disabled={increment === 0} onClick={() => {
+          setIncrement(i => i - 1);
+        }}>–</button>
+        <b>{increment}</b>
+        <button onClick={() => {
+          setIncrement(i => i + 1);
+        }}>+</button>
+      </p>
+    </>
+  );
+}
