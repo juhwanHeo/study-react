4af39651552f2c1cc5fb7436d0728164608bef1d
LSHgy <his5423@gmail.com>
2025-02-21 20:43:54 +0900

    feat: Add useFetchOptionData custom Hook
    - Add useFetchOptionData hook file
    - Apply useFetchOptionData custom hook in Task for place and planet API
    - Add SelectOptions function component in Task1.js

--- yiseoul/board-project/src/components/week8/task1/Task1.js
+++ yiseoul/board-project/src/components/week8/task1/Task1.js
@@ -1,51 +1,43 @@
-import { useState, useEffect } from 'react';
-import { fetchData } from './api.js';
+import { useFetchOptionData } from "./useFetchOptionData";
+
+function SelectOptions({ items, optionId, setOptionId }) {
+  return (
+    <select value={ optionId } onChange={ (e) => {
+      setOptionId(e.target.value);
+    }}>
+      { items.map(item =>
+          <option key={ item.id } value={ item.id }>
+            { item.name }
+          </option>
+      )}
+    </select>
+  );
+}
 
 export default function Page() {
-  const [planetList, setPlanetList] = useState([])
-  const [planetId, setPlanetId] = useState('');
-
-  const [placeList, setPlaceList] = useState([]);
-  const [placeId, setPlaceId] = useState('');
-
-  useEffect(() => {
-    let ignore = false;
-    fetchData('/planets').then(result => {
-      if (!ignore) {
-        console.log('Fetched a list of planets.');
-        setPlanetList(result);
-        setPlanetId(result[0].id); // 첫 번째 행성을 선택합니다.
-      }
-    });
-    return () => {
-      ignore = true;
-    }
-  }, []);
+  const [ planetList, planetId, setPlanetId ] = useFetchOptionData('/planets')
+  const [ placeList, placeId, setPlaceId ] = useFetchOptionData(planetId ? `/planets/${planetId}/places` : null) ;
 
   return (
       <>
         <label>
-          Pick a planet:{' '}
-          <select value={planetId} onChange={e => {
-            setPlanetId(e.target.value);
-          }}>
-            {planetList.map(planet =>
-                <option key={planet.id} value={planet.id}>{planet.name}</option>
-            )}
-          </select>
+          Pick a planet: &nbsp;
+          <SelectOptions
+            items = { planetList }
+            optionId = { planetId }
+            setOptionId = { setPlanetId }
+          />
         </label>
         <label>
-          Pick a place:{' '}
-          <select value={placeId} onChange={e => {
-            setPlaceId(e.target.value);
-          }}>
-            {placeList.map(place =>
-                <option key={place.id} value={place.id}>{place.name}</option>
-            )}
-          </select>
+          Pick a place: &nbsp;
+          <SelectOptions
+            items = { placeList }
+            optionId = { placeId }
+            setOptionId = { setPlaceId }
+          />
         </label>
-        <hr />
-        <p>You are going to: {placeId || '???'} on {planetId || '???'} </p>
+        <br />
+        <p>You are going to: { placeId || '???' } on { planetId || '???'} </p>
       </>
   );
 }
