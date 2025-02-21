bccc01458fff044accdd0e02a9720d7ef94b3f89
bobo-h <bo_asapap@kakao.com>
2025-02-21 23:05:15 +0900

    fix: add increment to useEffect dependency array to update count correctly

--- bohye/board-project/src/App.js
+++ bohye/board-project/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BoardProvider } from "./contexts/BoardContext";
 import Board from "./components/board/Board";
 import Task1 from "./components/week8/task1/Task1";
+import Task2 from "./components/week8/task2/Task2";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Board />
       </div>
       <Task1 />
+      <Task2 />
     </BoardProvider>
   );
 }
