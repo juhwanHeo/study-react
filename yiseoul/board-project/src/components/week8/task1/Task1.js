64c40aa1200cdb3790d4686da7e464393c9238ed
LSHgy <his5423@gmail.com>
2025-02-21 17:02:24 +0900

    feat: Add initial code for Task1, Task2 and connect components in App.js
    - Add initial code for Task1, Task2
    - Edit App.js to connect Task1 and Task2 components

---
+++ yiseoul/board-project/src/components/week8/task1/Task1.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { fetchData } from './api.js';
+
+export default function Page() {
+  const [planetList, setPlanetList] = useState([])
+  const [planetId, setPlanetId] = useState('');
+
+  const [placeList, setPlaceList] = useState([]);
+  const [placeId, setPlaceId] = useState('');
+
+  useEffect(() => {
+    let ignore = false;
+    fetchData('/planets').then(result => {
+      if (!ignore) {
+        console.log('Fetched a list of planets.');
+        setPlanetList(result);
+        setPlanetId(result[0].id); // 첫 번째 행성을 선택합니다.
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, []);
+
+  return (
+      <>
+        <label>
+          Pick a planet:{' '}
+          <select value={planetId} onChange={e => {
+            setPlanetId(e.target.value);
+          }}>
+            {planetList.map(planet =>
+                <option key={planet.id} value={planet.id}>{planet.name}</option>
+            )}
+          </select>
+        </label>
+        <label>
+          Pick a place:{' '}
+          <select value={placeId} onChange={e => {
+            setPlaceId(e.target.value);
+          }}>
+            {placeList.map(place =>
+                <option key={place.id} value={place.id}>{place.name}</option>
+            )}
+          </select>
+        </label>
+        <hr />
+        <p>You are going to: {placeId || '???'} on {planetId || '???'} </p>
+      </>
+  );
+}
---
+++ yiseoul/board-project/src/components/week8/task2/Task2.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function App() {
+  const [roomId, setRoomId] = useState('general');
+  const [isEncrypted, setIsEncrypted] = useState(false);
+  return (
+      <>
+        <label>
+          Choose the chat room:{' '}
+          <select
+              value={roomId}
+              onChange={e => setRoomId(e.target.value)}
+          >
+            <option value="general">general</option>
+            <option value="travel">travel</option>
+            <option value="music">music</option>
+          </select>
+        </label>
+        <label>
+          <input
+              type="checkbox"
+              checked={isEncrypted}
+              onChange={e => setIsEncrypted(e.target.checked)}
+          />
+          Enable encryption
+        </label>
+      </>
+  );
+}
