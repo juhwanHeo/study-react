acc18ffc7c7570b4a81ad462fdeab0203d93ac67
bobo-h <bo_asapap@kakao.com>
2025-02-21 22:56:15 +0900

    feat: useEffect to update place list when planet selection changes

--- bohye/board-project/src/App.js
+++ bohye/board-project/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BoardProvider } from "./contexts/BoardContext";
 import Board from "./components/board/Board";
+import Task1 from "./components/week8/task1/Task1";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="top">
         <Board />
       </div>
+      <Task1 />
     </BoardProvider>
   );
 }
