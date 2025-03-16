c77d8d4f29f9dece4cc1af481c6bdd02f39d5f8b
juhwanHeo <juhwanheo.dev@gmail.com>
2025-03-16 15:17:34 +0900

    feat: Resolve Task2

--- juhwan/board-project/src/App.tsx
+++ juhwan/board-project/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Board from "./components/board/Board";
 import Task1 from './components/week9/task1/Task1';
+import Task2 from './components/week9/task2/Task2';
 import { BoardProvider } from './context/BoardContext';
 import { ModalProvider } from './context/ModalContext';
 
@@ -13,6 +14,7 @@ function App() {
         </ModalProvider>
       </BoardProvider>
       <Task1 />
+      <Task2 />
     </div>
   );
 }
--- juhwan/board-project/src/components/week9/task2/Task2.tsx
+++ juhwan/board-project/src/components/week9/task2/Task2.tsx
@@ -1,12 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useCounter } from "./useCounter";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        const id = setInterval(() => {
-            setCount(c => c + 1);
-        }, 1000);
-        return () => clearInterval(id);
-    }, []);
+export default function Task2() {
+    const {count} = useCounter();
     return <h1>Seconds passed: {count}</h1>;
 }
