99d7fd32d0612303336d28598e7ff868eb50d223
LSHgy <his5423@gmail.com>
2025-03-20 23:33:36 +0900

    feat: Separate useState logic into useCount in Task2

--- yiseoul/board-project/src/components/week9/task2/useCounter.js
+++ yiseoul/board-project/src/components/week9/task2/useCounter.js
@@ -1,1 +1,12 @@
-// Write your custom Hook in this file!
+import { useState, useEffect } from 'react';
+
+export function useCounter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return [count];
+}
