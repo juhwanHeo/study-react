7b65d8908c0d902700d2097e1efd42f3f08b6864
bobo-h <166573095+bobo-h@users.noreply.github.com>
2025-03-22 09:28:34 +0900

    feat: clone task2

---
+++ bohye/board-project/src/components/week9/task2/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return <h1>Seconds passed: {count}</h1>;
+}
