e6ade17e77d92aada7e2bddd4fd98bd8ed7f03e3
juwonlee-dev <juwonlee.dev@gmail.com>
2025-03-25 02:02:44 +0900

    refactor: resolve Task2

--- juwonlee/board-project/src/components/week9/task2/Task2.js
+++ juwonlee/board-project/src/components/week9/task2/Task2.js
@@ -1,12 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useCounter } from './useCounter.js';
 
 export default function Task2() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount(c => c + 1);
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
+  const count = useCounter();
   return <h1>Seconds passed: {count}</h1>;
 }
