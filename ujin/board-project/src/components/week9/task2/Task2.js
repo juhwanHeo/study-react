d297fd2ee6140714d4cedd986f339774a06d3050
MIK_WJ <congbyrr@gmail.com>
2025-03-25 01:02:05 +0900

    [FEAT] Init week9

---
+++ ujin/board-project/src/components/week9/task2/Task2.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return <h1>Seconds passed: {count}</h1>;
+}
