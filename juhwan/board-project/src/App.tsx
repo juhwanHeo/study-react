555fd1cf196669ca75e4d350c12b24317b4d91ec
juhwanHeo <juhwanheo.dev@gmail.com>
2025-05-06 16:19:14 +0900

    chore: Update double quote to single quote for import

--- juhwan/board-project/src/App.tsx
+++ juhwan/board-project/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Board from "./components/board/Board";
+import Board from './components/board/Board';
 import Task1 from './components/week9/task1/Task1';
 import Task2 from './components/week9/task2/Task2';
 import { BoardProvider } from './context/BoardContext';
