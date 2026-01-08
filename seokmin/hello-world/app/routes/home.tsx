518bc8e6eae6354e306e722966ad3cb365e40864
toryu4765 <toryu4765>
2026-01-09 00:58:15 +0900

    feat: init project

---
+++ seokmin/hello-world/app/routes/home.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/home";
+import Profile from "../src/components/Profile";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Profile />;
+}
