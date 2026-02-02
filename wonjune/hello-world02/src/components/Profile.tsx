61e429702fa397e697043e2b5f758667312382f3
wonjunejang <asd990505@gmail.com>
2026-02-02 22:47:46 +0900

    feat:Profile 꾸미기

--- wonjune/hello-world02/src/components/Profile.tsx
+++ wonjune/hello-world02/src/components/Profile.tsx
@@ -1,13 +1,13 @@
-//이름, 나이, 생년월일을 출력하는 컴포넌트입니다.
 export default function Profile() {
   const name = "장원준";
   const age = "28";
   const birthday = "1999.05.05";
+  const textStyle = {color:'white', fontWeight:'bold'};
   return (
-    <>
-      <p>이름:{name}</p>
-      <p>나이:{age}</p>
-      <p>생년월일:{birthday}</p>
-    </>
+    <div style={{background:'blue', borderRadius:'25%', padding:'10px'}}>
+      <p style={textStyle}>이름:{name}</p>
+      <p style={textStyle}>나이:{age}</p>
+      <p style={textStyle}>생년월일:{birthday}</p>
+    </div>
   );
 }
