cf22e417ba0c4ee8fa7e5cd083f80446e2008753
zzik_sh <happytngus1@gmail.com>
2026-02-03 21:02:05 +0900

    fix: fix indent3

--- suhyeon/hello-world02/src/App.tsx
+++ suhyeon/hello-world02/src/App.tsx
@@ -3,19 +3,19 @@ import './App.css'
 import Profile from './components/Profile';
 
 function App() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
-    return (
-        <>
-            <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMglPfQOzVWxMsb67lTUwcnl-kuv4w1Diqg&s" className="logo" alt="Vite logo" />
-            </div>
-            <h1>자기소개 컴포넌트</h1>
-            <div className="card">
-                <Profile />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMglPfQOzVWxMsb67lTUwcnl-kuv4w1Diqg&s" className="logo" alt="Vite logo" />
+      </div>
+      <h1>자기소개 컴포넌트</h1>
+      <div className="card">
+        <Profile />
+      </div>
+    </>
+  )
 }
 
 export default App
