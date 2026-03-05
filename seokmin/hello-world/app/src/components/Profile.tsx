85887cdef0f51bae7a6482ee2824c68bbbe65833
toryu4765 <toryu4765>
2026-03-06 00:00:30 +0900

    feat(task3): 조건부 렌더링

--- seokmin/hello-world/app/src/components/Profile.tsx
+++ seokmin/hello-world/app/src/components/Profile.tsx
@@ -2,9 +2,10 @@ type ProfileProps = {
   name: string
   age: number
   birth: string
+  isCheck: boolean
 }
 
-export default function Profile({ name, age, birth }: ProfileProps) {
+export default function Profile({ name, age, birth, isCheck }: ProfileProps) {
   const userData = {
     photo: "thsm0331",
   }
@@ -15,18 +16,23 @@ export default function Profile({ name, age, birth }: ProfileProps) {
       , flexDirection: 'column'
       , alignItems: 'center'
       , justifyContent: 'center'
-      , minHeight: '100vh'
-      , background: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)'
+      , padding: '1.6rem'
+      , margin: '1rem 10rem'
+      , borderRadius: '1rem'
+      , border: isCheck ? '2px solid #25343F' : '2px solid #FF9B51'
+      , color: isCheck ? '#25343F' : '#FF9B51'
     }}>
       <h1>프로필</h1>
-      <img
-        src={`https://github.com/${userData.photo}.png`}
-        alt=''
-        style={styles.image}
-      />
-      <p>이름: {name}</p>
-      <p>나이: {age}</p>
-      <p>생년월일: {birth}</p>
+
+      {isCheck ? (
+        <>
+          <p>이름: {name}</p>
+          <p>나이: {age}</p>
+          <p>생년월일: {birth}</p>
+        </>
+      ) : (
+        <p>이름: {name}</p>
+      )}
     </div>
   )
 }
